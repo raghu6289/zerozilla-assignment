4e858c31126469a43d717cfad5864eaf82cbbdb3
raghu6289 <raghua7795@gmail.com>
2022-04-03 15:54:11 +0530

    Modified in errorHandler

--- app/middleware/globalErrorHandler.js
+++ app/middleware/globalErrorHandler.js
@@ -15,9 +15,9 @@ export default (error, req, res, next) => {
     case errorCodes.validationError:
       return res.status(401).send('All fields are required')
     case errorCodes.usernameValidation:
-      return res.status(401).send('"UserName must be at least 3"')
+      return res.status(401).send('UserName must be at least 3')
     case errorCodes.passwordValidation:
-      return res.status(401).send('"Password must be at least 6"')
+      return res.status(401).send('Password must be at least 6')
     default:
       return res.status(500).send('Unknown error')
   }
