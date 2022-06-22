197e300cfa6031909efabee85611c9ddc72d7950
raghua7795 <raghua7795@gmail.com>
2022-06-22 23:44:32 +0530

    CON

--- app/database/config.js
+++ app/database/config.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 // Mongodb connection
 async function mongoSetup() {
-  mongoose.connect("mongodb+srv://raghu7795:Password%237795@cluster0.y7cad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    mongoose.connect("mongodb://localhost:27017/zerozilladb",
+  // mongoose.connect("mongodb+srv://raghu7795:Password%237795@cluster0.y7cad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
+    {
   });
 }
 
