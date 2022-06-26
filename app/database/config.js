70ff37663f3dc1b0c45162088b32013a61cdf924
raghua7795 <raghua7795@gmail.com>
2022-06-26 20:42:46 +0530

    env

--- app/database/config.js
+++ app/database/config.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
+import "dotenv/config"
 
 // Mongodb connection
 async function mongoSetup() {
-    mongoose.connect("mongodb://localhost:27017/zerozilladb",
-  // mongoose.connect("mongodb+srv://raghu7795:Password%237795@cluster0.y7cad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
-    {
-  });
+  mongoose.connect(process.env.CONN, (err) => {
+    if (err) console.log("DB Failed to Connect");
+    console.log("DB Connected Successfully");
+    }
+  // mongoose.connect("mongodb+srv://raghu7795:Password%237795@cluster0.y7cad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  );
 }
 
 export default mongoSetup;
